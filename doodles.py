8636f9ef18aa717aa224165d1a0bbe06e3395ed2
Joshua Li <joshuazli2020@gmail.com>
2023-01-24 14:24:40 +1000

    preprocessing the data

--- doodles.py
+++ doodles.py
@@ -2,6 +2,36 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
+class_names = ['bee', 'butterfly', 'eye', 'mushroom', 'diamond', 'snowman', 'cat', 'popsicle', 'octopus', 'bowtie']
+
+image_size = (28, 28)
+
+ds_train = tf.keras.preprocessing.image_dataset_from_directory(
+    'images',
+    labels='inferred',
+    label_mode='int',
+    class_names=class_names,
+    color_mode='grayscale',
+    batch_size=64,
+    image_size=image_size,
+    seed=123,
+    validation_split=0.2,
+    subset='training',
+)
+
+ds_validation = tf.keras.preprocessing.image_dataset_from_directory(
+    'images',
+    labels='inferred',
+    label_mode='int',
+    class_names=class_names,
+    color_mode='grayscale',
+    batch_size=64,
+    image_size=image_size,
+    seed=123,
+    validation_split=0.2,
+    subset='validation',
+)
+
 img_height = 28
 img_width = 28
 
@@ -14,4 +44,3 @@ model = tf.keras.Sequential([
 model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
-                
